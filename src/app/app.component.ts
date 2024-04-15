275c6195a451b8b9c6e442a7e01f96ed0c28a0e0
VIS-WAS <vishwas.ms8830@gmail.com>
2024-04-15 13:40:40 +0530

    retrieving form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,16 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   title = 'template-driven-form';
 
-  firstName: string = 'Vishwas';
-  lastname: string = 'MS';
+  firstName: string = '';
+  lastname: string = '';
   mail: string = '';
-  dob: string = '10/04/2024';
+  dob: string = '';
+  gender: string = '';
+  country: string = '';
+  city: string = '';
+  region: string = '';
+  postal: string = '';
+  username: string = '';
 
   defaultGender: string = 'male';
   defaultCountry: string = 'India';
@@ -38,13 +44,16 @@ export class AppComponent {
 
   OnFormSubmit() {
     console.log(this.form);
-    // console.log(this.form.controls['firstname'].value);
-    // console.log(this.form.value.lastname);
-    // console.log(this.form.value.email);
-    // console.log(this.form.value.country);
-    // console.log(this.form.value.gender);
-    // console.log(this.form.value.address.country);
-    // console.log(this.dob);
+
+    this.firstName = this.form.value.firstname;
+    this.lastname = this.form.value.lastname;
+    this.mail = this.form.value.email;
+    this.country = this.form.value.address.country;
+    this.region = this.form.value.address.region;
+    this.city = this.form.value.address.city;
+    this.postal = this.form.value.address.postal;
+    this.username = this.form.value.username;
+    this.dob = this.form.value.dob;
 
     this.resetForm();
   }
@@ -59,41 +68,24 @@ export class AppComponent {
   }
 
   generateUserName() {
+    // this.firstName = this.form.value.firstname;
+    // this.lastname = this.form.value.lastname;
+
+    // this.dob = this.form.value.dob;
     let username = '';
-    if (this.firstName.length >= 3) {
-      username += this.firstName.slice(0, 3);
+    if (this.form.value.firstname.length >= 3) {
+      username += this.form.value.firstname.slice(0, 3);
     } else {
-      username += this.firstName;
+      username += this.form.value.firstname;
     }
-    if (this.lastname.length >= 3) {
-      username += this.lastname.slice(0, 3);
+    if (this.form.value.lastname.length >= 3) {
+      username += this.form.value.lastname.slice(0, 3);
     } else {
-      username += this.lastname;
+      username += this.form.value.lastname;
     }
-    let datetime = new Date(this.dob);
+    let datetime = new Date(this.form.value.dob);
     username += datetime.getFullYear();
     username = username.toLowerCase();
-
-    //this.form.value.username = username   /// property value is updated but it will not be populated in formcontrol
-    // this.form.controls['username'].value = username;     // will throw error -> read-only
-
-    // this.form.setValue({
-    //   address: {
-    //     city: this.form.value.address.city,
-    //     country: this.form.value.address.country,
-    //     postal: this.form.value.address.postal,
-    //     region: this.form.value.address.region,
-    //     street1: this.form.value.address.street1,
-    //     street2: this.form.value.address.street2,
-    //   },
-    //   dob: this.form.value.dob,
-    //   email: this.form.value.email,
-    //   firstname: this.form.value.firstname,
-    //   gender: this.form.value.gender,
-    //   lastname: this.form.value.lastname,
-    //   phone: this.form.value.phone,
-    //   username: username,
-    // });
 
     this.form.form.patchValue({
       username: username,
